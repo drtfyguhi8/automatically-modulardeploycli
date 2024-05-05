4d3fc8f2cb3c3e55d2cd9f4cba537d9a6be2c872
drtfyguhi8 <js1grogodil@hotmail.com>
2024-05-05 21:17:41 +0700

    demo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function capitalizeWords(str) {
-  return str
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
+const pullAtValue = (arr, pullArr) => {
+  let removed = [],
+    pushToRemove = arr.forEach((v, i) =>
+      pullArr.includes(v) ? removed.push(v) : v,
+    ),
+    mutateTo = arr.filter((v, i) => !pullArr.includes(v));
+  arr.length = 0;
+  mutateTo.forEach((v) => arr.push(v));
+  return removed;
+};
