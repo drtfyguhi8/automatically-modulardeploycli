9d04f6dc549c12df9932e22bec97bedb48600b40
drtfyguhi8 <js1grogodil@hotmail.com>
2024-05-06 14:05:12 +0700

    update task

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const pullAtValue = (arr, pullArr) => {
-  let removed = [],
-    pushToRemove = arr.forEach((v, i) =>
-      pullArr.includes(v) ? removed.push(v) : v,
-    ),
-    mutateTo = arr.filter((v, i) => !pullArr.includes(v));
-  arr.length = 0;
-  mutateTo.forEach((v) => arr.push(v));
-  return removed;
-};
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
